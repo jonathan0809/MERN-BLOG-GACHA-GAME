f727930194be226dce9a1c5af5f0bfe9b7d41cf7
jonah oswald <kmapandin@gmail.com>
2024-02-18 14:41:43 +0900

    done guess

--- api/index.js
+++ api/index.js
@@ -1,3 +1,4 @@
+// Import dependencies
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -14,95 +15,129 @@ const Post = require("./models/Post");
 const jwt = require("jsonwebtoken");
 const secret = "kjfnkauerhafba8372i1";
 
+// Configure middleware
 app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/uploads", express.static(__dirname + "/uploads"));
 
+// Connect to database
 mongoose.connect(
   "mongodb+srv://gachaBlogMern:NcOboLtwBySVA9Bw@cluster0.m5omvbh.mongodb.net/?retryWrites=true&w=majority"
 );
 
-app.post("/register", async (req, res) => {
-  const { username, password } = req.body;
-  try {
+app.post('/register', async (req,res) => {
+  const {username,password} = req.body;
+  try{
     const userDoc = await User.create({
       username,
-      password: bcrypt.hashSync(password, salt),
+      password:bcrypt.hashSync(password,salt),
     });
     res.json(userDoc);
-  } catch (e) {
+  } catch(e) {
+    console.log(e);
     res.status(400).json(e);
   }
 });
 
-app.post("/login", async (req, res) => {
-  const { username, password } = req.body;
-  const userDoc = await User.findOne({ username });
-  const passwordMatch = bcrypt.compareSync(password, userDoc.password);
-  // res.json(passwordMatch);
-  if (passwordMatch) {
-    // loggin successful
-    jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
+app.post('/login', async (req,res) => {
+  const {username,password} = req.body;
+  const userDoc = await User.findOne({username});
+  const passOk = bcrypt.compareSync(password, userDoc.password);
+  if (passOk) {
+    // logged in
+    jwt.sign({username,id:userDoc._id}, secret, {}, (err,token) => {
       if (err) throw err;
-      res.cookie("token", token).json({
-        id: userDoc._id,
+      res.cookie('token', token).json({
+        id:userDoc._id,
         username,
       });
     });
-    // res.json({});
   } else {
-    res.status(400).json("Invalid username or password");
+    res.status(400).json('wrong credentials');
   }
 });
 
-app.get("/profile", (req, res) => {
-  const { token } = req.cookies;
-  jwt.verify(token, secret, {}, (err, info) => {
+app.get('/profile', (req,res) => {
+  const {token} = req.cookies;
+  jwt.verify(token, secret, {}, (err,info) => {
     if (err) throw err;
     res.json(info);
   });
 });
 
-app.post("/logout", (req, res) => {
-  res.cookie("token", "").json("ok");
+app.post('/logout', (req,res) => {
+  res.cookie('token', '').json('ok');
 });
 
-app.post("/post", uploadMiddleware.single("files"), async (req, res) => {
-  const { originalname, path } = req.file;
-  const parts = originalname.split(".");
+app.post('/post', uploadMiddleware.single('file'), async (req,res) => {
+  const {originalname,path} = req.file;
+  const parts = originalname.split('.');
   const ext = parts[parts.length - 1];
-  const newPath = path + "." + ext;
+  const newPath = path+'.'+ext;
   fs.renameSync(path, newPath);
 
-  const { token } = req.cookies;
-  jwt.verify(token, secret, {}, async (err, info) => {
+  const {token} = req.cookies;
+  jwt.verify(token, secret, {}, async (err,info) => {
     if (err) throw err;
-    const { title, summary, content } = req.body;
+    const {title,summary,content} = req.body;
     const postDoc = await Post.create({
       title,
       summary,
       content,
-      cover: newPath,
-      author: info.id,
+      cover:newPath,
+      author:info.id,
     });
     res.json(postDoc);
   });
+
 });
 
-app.get("/post", async (req, res) => {
+app.put('/post',uploadMiddleware.single('file'), async (req,res) => {
+  let newPath = null;
+  if (req.file) {
+    const {originalname,path} = req.file;
+    const parts = originalname.split('.');
+    const ext = parts[parts.length - 1];
+    newPath = path+'.'+ext;
+    fs.renameSync(path, newPath);
+  }
+
+  const {token} = req.cookies;
+  jwt.verify(token, secret, {}, async (err,info) => {
+    if (err) throw err;
+    const {id,title,summary,content} = req.body;
+    const postDoc = await Post.findById(id);
+    const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
+    if (!isAuthor) {
+      return res.status(400).json('you are not the author');
+    }
+    await postDoc.update({
+      title,
+      summary,
+      content,
+      cover: newPath ? newPath : postDoc.cover,
+    });
+
+    res.json(postDoc);
+  });
+
+});
+
+app.get('/post', async (req,res) => {
   res.json(
     await Post.find()
-    .populate("author", ["username"])
-    .sort({createdAt: -1})
-    .limit(10)
-    );
+      .populate('author', ['username'])
+      .sort({createdAt: -1})
+      .limit(20)
+  );
 });
 
-app.get("/post/:id", async (req, res) =>{
+app.get('/post/:id', async (req, res) => {
   const {id} = req.params;
-  const postDoc = await Post.findById(id).populate("author", ["username"]);
+  const postDoc = await Post.findById(id).populate('author', ['username']);
   res.json(postDoc);
 })
 
 app.listen(4000);
+//
--- client/src/Pages/EditPost.js
+++ client/src/Pages/EditPost.js
@@ -3,15 +3,15 @@ import { Navigate, useParams } from "react-router-dom";
 import Editor from "../Editor";
 
 export default function EditPost() {
-    const {id} = useParams();
-    const [title, setTitle] = useState("");
-    const [summary, setSummary] = useState("");
-    const [content, setContent] = useState("");
-    const [files, setFiles] = useState("");
-    const [redirect, setRedirect] = useState("");
+  const { id } = useParams();
+  const [title, setTitle] = useState("");
+  const [summary, setSummary] = useState("");
+  const [content, setContent] = useState("");
+  const [files, setFiles] = useState("");
+  const [redirect, setRedirect] = useState(false);
 
-    useEffect(() => {
-    fetch("http://localhost:4000/post/"+id)
+  useEffect(() => {
+    fetch("http://localhost:4000/post/" + id)
       .then((response) => response.json())
       .then((postInfo) => {
         setTitle(postInfo.title);
@@ -20,31 +20,46 @@ export default function EditPost() {
       });
   }, []);
 
-
-    function updatePost(ev){
-        ev.preventDefault();
+  async function updatePost(ev) {
+    ev.preventDefault();
+    const data = new FormData();
+    data.set("title", title);
+    data.set("summary", summary);
+    data.set("content", content);
+    if (files?.[0]){
+      data.set("files", files?.[0]);
+    }
+    const response = await fetch("http://localhost:4000/post", {
+      method: "put",
+      body: data,
+      credentials: "include",
+    });
+    if (response.ok) {
+      setRedirect(true);
     }
 
-    if (redirect) {
-        return <Navigate to={"/"} />;
-      }
-      return (
-        <form onSubmit={updatePost}>
-          <input
-            type="text"
-            placeholder={"Title"}
-            value={title}
-            onChange={(ev) => setTitle(ev.target.value)}
-          />
-          <input
-            type="summary"
-            placeholder={"Summary"}
-            value={summary}
-            onChange={(ev) => setSummary(ev.target.value)}
-          />
-          <input type="file" onChange={(ev) => setFiles(ev.target.files)} />
-          <Editor value={content} onChange={setContent} />
-          <button style={{ marginTop: "5px" }}>Create Post</button>
-        </form>
-      );
+  }
+
+  if (redirect) {
+    return <Navigate to={"/post/"+id} />;
+  }
+  return (
+    <form onSubmit={updatePost}>
+      <input
+        type="text"
+        placeholder={"Title"}
+        value={title}
+        onChange={(ev) => setTitle(ev.target.value)}
+      />
+      <input
+        type="summary"
+        placeholder={"Summary"}
+        value={summary}
+        onChange={(ev) => setSummary(ev.target.value)}
+      />
+      <input type="file" onChange={(ev) => setFiles(ev.target.files)} />
+      <Editor value={content} onChange={setContent} />
+      <button style={{ marginTop: "5px" }}>Update Post</button>
+    </form>
+  );
 }
